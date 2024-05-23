70304c521fb26db7fe5a85d9deac80ba72538290
ilovemane <mike.du@wolfson.ox.ac.uk>
2024-05-23 15:52:40 +0100

    update

--- R/mockPerson.R
+++ R/mockPerson.R
@@ -1,28 +1,29 @@
 #' Generates a mock person table and integrates it into an existing CDM object.
 #'
-#' This function creates a mock person table with specified characteristics for each individual, including a randomly assigned date of birth within a given range and gender based on specified proportions. It populates the CDM object's person table with these entries, ensuring each record is uniquely identified.
+#' This function creates a mock person table with specified characteristics for each individual,
+#' including a randomly assigned date of birth within a given range and gender based on specified
+#' proportions. It populates the CDM object's person table with these entries, ensuring each record
+#' is uniquely identified.
 #'
 #' @param cdm A `cdm_reference` object that serves as the base structure for adding the person table.
-#'            This parameter should be an existing or newly created CDM object that does not yet contain a 'person' table.
-#'
+#'            This parameter should be an existing or newly created CDM object that does not yet
+#'            contain a 'person' table.
 #' @param nPerson An integer specifying the number of mock persons to create in the person table.
-#'                This defines the scale of the simulation and allows for the creation of datasets with varying sizes.
-#'
+#'                This defines the scale of the simulation and allows for the creation of datasets
+#'                with varying sizes.
 #' @param birthRange A date range within which the birthdays of the mock persons will be randomly generated.
-#'                   This should be provided as a vector of two dates (`as.Date` format), specifying the start and end of the range.
-#'                   For example, `as.Date(c("1950-01-01", "2000-12-31"))` specifies that all generated birthdays will fall between January 1, 1950, and December 31, 2000.
+#'                   This should be provided as a vector of two dates (`as.Date` format), specifying
+#'                   the start and end of the range.
+#' @param proportionFemale A numeric value between 0 and 1 indicating the proportion of the persons
+#'                         who are female. For example, a value of 0.5 means approximately 50% of
+#'                         the generated persons will be female. This helps simulate realistic demographic distributions.
+#' @param seed An optional integer used to set the seed for random number generation, ensuring
+#'             reproducibility of the generated data. If provided, this seed allows the function
+#'             to produce consistent results each time it is run with the same inputs. If 'NULL',
+#'             the seed is not set, which can lead to different outputs on each run.
 #'
-#' @param proportionFemale A numeric value between 0 and 1 indicating the proportion of the persons who are female.
-#'                         For example, a value of 0.5 means approximately 50% of the generated persons will be female. This helps simulate realistic demographic distributions.
-#'
-#' @param seed An optional integer used to set the seed for random number generation, ensuring reproducibility of the generated data.
-#'             If provided, this seed allows the function to produce consistent results each time it is run with the same inputs.
-#'             If 'NULL', the seed is not set, which can lead to different outputs on each run.
-#'
-#' @return Returns the modified `cdm` object with the new 'person' table added. This table includes simulated
+#' @return A modified `cdm` object with the new 'person' table added. This table includes simulated
 #'         person data for each generated individual, with unique identifiers and demographic attributes.
-#'
-#' @export
 #'
 #' @examples
 #' \donttest{
@@ -32,6 +33,9 @@
 #' # View the generated person data
 #' print(cdm$person)
 #' }
+#'
+#' @export
+
 mockPerson <- function(cdm = mockCdmReference(),
                        nPerson = 10,
                        birthRange = as.Date(c("1950-01-01", "2000-12-31")),
