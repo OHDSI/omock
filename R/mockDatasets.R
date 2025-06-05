e5b4ce3c8baa9b027ec2a48f826da2ac909ae184
catalamarti <marticatalasabate@gmail.com>
2025-06-05 23:58:22 +0100

    add functions to download mock datasets

--- R/mockDatasets.R
+++ R/mockDatasets.R
@@ -1,4 +1,75 @@
 
+#' Create a `local` cdm_reference from a dataset.
+#'
+#' @param datasetName Name of the mock dataset. See `availableMockDatasets()`
+#' for possibilities.
+#'
+#' @return A local cdm_reference object.
+#' @export
+#'
+#' @examples
+#' library(omock)
+#'
+#' mockDatasetsFolder(tempdir())
+#' downloadMockDataset(datasetName = "GiBleed")
+#' cdm <- mockCdmFromDataset(datasetName = "GiBleed")
+#' cdm
+#'
+mockCdmFromDataset <- function(datasetName = "GiBleed") {
+  # initial check
+  datasetName <- validateDatasetName(datasetName)
+  cn <- omock::mockDatasets$cdm_name[omock::mockDatasets$dataset_name == datasetName]
+  cv <- omock::mockDatasets$cdm_version[omock::mockDatasets$dataset_name == datasetName]
+
+  # make dataset available
+  datasetPath <- datasetAvailable(datasetName)
+
+  # folder to unzip
+  tmpFolder <- file.path(tempdir(), omopgenerics::uniqueId())
+  if (dir.exists(tmpFolder)) {
+    unlink(x = tmpFolder, recursive = FALSE)
+  }
+  dir.create(tmpFolder)
+
+  # unzip
+  utils::unzip(zipfile = datasetPath, exdir = tmpFolder)
+  tables <- readTables(tmpFolder, cv)
+
+  # delete csv files
+  unlink(x = tmpFolder, recursive = TRUE)
+
+  omopgenerics::cdmFromTables(tables = tables, cdmName = cn, cdmVersion = cv)
+}
+readTables <- function(tmpFolder, cv) {
+  tables <- list.files(tmpFolder, full.names = TRUE, pattern = "\\.csv$")
+  names(tables) <- substr(basename(tables), 1, nchar(basename(tables)) - 4)
+  tables <- as.list(tables)
+  x <- omopgenerics::omopTableFields(cdmVersion = cv)
+  for (nm in names(tables)) {
+    file <- tables[[nm]]
+    cols <- stringr::str_split_1(readLines(con = file, n = 1), ",")
+    colType <- x |>
+      dplyr::filter(
+        .data$cdm_table_name == .env$nm, .data$cdm_field_name %in% .env$cols
+      ) |>
+      dplyr::mutate(cdm_datatype = dplyr::case_when(
+        .data$cdm_datatype == "integer" ~ "i",
+        .data$cdm_datatype == "datetime" ~ "T",
+        .data$cdm_datatype == "date" ~ "D",
+        .data$cdm_datatype == "float" ~ "d",
+        .data$cdm_datatype == "logical" ~ "l",
+        stringr::str_detect(.data$cdm_datatype, "varchar") ~ "c"
+      ))
+    colTypes <- rlang::set_names(as.list(colType$cdm_datatype), colType$cdm_field_name)
+    missingTypes <- cols[!cols %in% names(colTypes)]
+    for (mt in missingTypes) {
+      colTypes[[mt]] <- "?"
+    }
+    tables[[nm]] <- readr::read_csv(file = file, col_types = colTypes)
+  }
+  tables
+}
+
 #' Available mock OMOP CDM Synthetic Datasets
 #'
 #' These are the mock OMOP CDM Synthetic Datasets that are available to download
@@ -18,3 +89,200 @@
 #' mockDatasets
 #'
 "mockDatasets"
+
+#' Download an OMOP Synthetic dataset.
+#'
+#' @param datasetName Name of the mock dataset. See `availableMockDatasets()`
+#' for possibilities.
+#' @param path Path where to download the dataset.
+#' @param overwrite Whether to overwrite the dataset if it is already
+#' downloaded.
+#'
+#' @return The path to the downloaded dataset.
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' library(omock)
+#'
+#' isMockDatasetDownloaded("GiBleed")
+#' downloadMockDataset("GiBleed")
+#' isMockDatasetDownloaded("GiBleed")
+#' }
+#'
+downloadMockDataset <- function(datasetName = "GiBleed",
+                                path = mockDatasetsFolder(),
+                                overwrite = FALSE) {
+  # initial checks
+  datasetName <- validateDatasetName(datasetName)
+  path <- validatePath(path)
+  omopgenerics::assertLogical(overwrite, length = TRUE)
+
+  datasetFile <- file.path(path, paste0(datasetName, ".zip"))
+  # is available
+  if (file.exists(datasetFile)) {
+    if (overwrite) {
+      file.remove(datasetFile)
+    } else {
+      if (rlang::is_interactive()) {
+        cli::cli_inform(c(i = "Set `overwrite = TRUE` to silence this message."))
+      }
+      if (question("Do you want to overwrite prior existing dataset? Y/n")) {
+        file.remove(datasetFile)
+      } else {
+        return(invisible(datasetFile))
+      }
+    }
+  }
+
+  # download dataset
+  url <- omock::mockDatasets$url[omock::mockDatasets$dataset_name == datasetName]
+  utils::download.file(url = url, destfile = datasetFile)
+
+  invisible(datasetFile)
+}
+
+#' Check if a certain dataset is downloaded.
+#'
+#' @param datasetName Name of the mock dataset. See `availableMockDatasets()`
+#' for possibilities.
+#' @param path Path where to search for the dataset.
+#'
+#' @return Whether the dataset is available or not.
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' library(omock)
+#'
+#' isMockDatasetDownloaded("GiBleed")
+#' downloadMockDataset("GiBleed")
+#' isMockDatasetDownloaded("GiBleed")
+#' }
+#'
+isMockDatasetDownloaded <- function(datasetName = "GiBleed",
+                                    path = mockDatasetsFolder()) {
+  # initial checks
+  datasetName <- validateDatasetName(datasetName)
+  path <- validatePath(path)
+
+  file.exists(file.path(path, paste0(datasetName, ".zip")))
+}
+
+#' List the available datasets
+#'
+#' @return A character vector with the available datasets.
+#' @export
+#'
+#' @examples
+#' library(omock)
+#'
+#' availableMockDatasets()
+#'
+availableMockDatasets <- function() {
+  omock::mockDatasets$dataset_name
+}
+
+#' Check the availability of the OMOP CDM datasets.
+#'
+#' @return A message with the availability of the datasets.
+#' @export
+#'
+#' @examples
+#' library(omock)
+#'
+#' mockDatasetsStatus()
+#'
+mockDatasetsStatus <- function() {
+  x <- omock::mockDatasets |>
+    dplyr::select("dataset_name") |>
+    dplyr::mutate(exists = dplyr::if_else(file.exists(file.path(
+      mockDatasetsFolder(), .data$dataset_name, paste0(.data$dataset_name, ".zip")
+    )), 1, 0)) |>
+    dplyr::arrange(dplyr::desc(.data$exists), .data$dataset_name) |>
+    dplyr::mutate(status = dplyr::if_else(.data$exists == 1, "v", "x"))
+  cli::cli_inform(rlang::set_names(x = x$dataset_name, nm = x$status))
+  invisible(x)
+}
+
+#' Check or set the datasets Folder
+#'
+#' @param path Path to a folder to store the synthetic datasets. If NULL the
+#' current OMOP_DATASETS_FOLDER is returned.
+#'
+#' @return The dataset folder.
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' mockDatasetsFolder()
+#' mockDatasetsFolder(file.path(tempdir(), "OMOP_DATASETS"))
+#' mockDatasetsFolder()
+#' }
+#'
+mockDatasetsFolder <- function(path = NULL) {
+  if (is.null(path)) {
+    if (Sys.getenv(mockDatasetsKey) == "") {
+      tempMockDatasetsFolder <- file.path(tempdir(), mockDatasetsKey)
+      dir.create(tempMockDatasetsFolder, showWarnings = FALSE)
+      if (rlang::is_interactive()) {
+        cli::cli_inform(c("i" = "`{mockDatasetsKey}` temporarily set to {.path {tempMockDatasetsFolder}}."))
+        cli::cli_inform(c("!" = "Please consider creating a permanent `{mockDatasetsKey}` location."))
+      }
+      arg <- rlang::set_names(x = tempMockDatasetsFolder, nm = mockDatasetsKey)
+      do.call(what = Sys.setenv, args = as.list(arg))
+    }
+    return(Sys.getenv(mockDatasetsKey))
+  } else {
+    omopgenerics::assertCharacter(x = path, length = 1)
+    if (!dir.exists(path)) {
+      cli::cli_inform(c("i" = "Creating {.path {path}}."))
+      dir.create(path)
+    }
+    arg <- rlang::set_names(x = path, nm = mockDatasetsKey)
+    do.call(what = Sys.setenv, args = as.list(arg))
+    if (rlang::is_interactive()) {
+      c("i" = "If you want to create a permanent `{mockDatasetsKey}` write the following in your `.Renviron` file:",
+        "", " " = "{.pkg {mockDatasetsKey}}=\"{path}\"", "") |>
+        cli::cli_inform()
+    }
+    return(invisible(Sys.getenv(mockDatasetsKey)))
+  }
+}
+
+datasetAvailable <- function(datasetName, call = parent.frame()) {
+  folder <- mockDatasetsFolder()
+  if (!isMockDatasetDownloaded(datasetName = datasetName, path = folder)) {
+    if (question(paste0("`", datasetName, "` is not downloaded, do you want to download it? Y/n"))) {
+      downloadMockDataset(datasetName = datasetName, path = folder)
+    } else {
+      cli::cli_abort(c(x = "`{datasetName}` is not downloaded."), call = call)
+    }
+  }
+  file.path(folder, paste0(datasetName, ".zip"))
+}
+question <- function(message) {
+  if (rlang::is_interactive()) {
+    x <- ""
+    while(!x %in% c("yes", "no")) {
+      cli::cli_inform(message = message)
+      x <- tolower(readline())
+      x[x == "y"] <- "yes"
+      x[x == "n"] <- "no"
+    }
+    x == "yes"
+  } else {
+    TRUE
+  }
+}
+validateDatasetName <- function(datasetName, call = parent.frame()) {
+  omopgenerics::assertChoice(datasetName, choices = availableMockDatasets(), length = 1, call = call)
+  invisible(datasetName)
+}
+validatePath <- function(path, call = parent.frame()) {
+  omopgenerics::assertCharacter(x = path, length = 1, call = call)
+  if (!dir.exists(path)) {
+    cli::cli_abort(c(x = "Path {.path {path}} does not exist."), call = call)
+  }
+  invisible(path)
+}
