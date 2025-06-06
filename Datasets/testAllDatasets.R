b016f838e1590364dd7629552db504666699ce18
catalamarti <marticatalasabate@gmail.com>
2025-06-06 12:11:44 +0100

    typo

--- Datasets/testAllDatasets.R
+++ Datasets/testAllDatasets.R
@@ -1,6 +1,6 @@
 
 devtools::load_all()
-nm <- availableMockDatasets()
+nms <- availableMockDatasets()
 for (nm in nms) {
   time <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
   cli::cli_inform(c("i" = "{time} Creating Dataset {nm}."))
