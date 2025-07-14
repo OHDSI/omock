3664b388a5cc7b5af09c73f04d7ed86f2c1a0ee6
ilovemane <58779940+ilovemane@users.noreply.github.com>
2025-07-14 08:53:29 +0100

    Update test-mockVocabularySet.R

--- tests/testthat/test-mockVocabularySet.R
+++ tests/testthat/test-mockVocabularySet.R
@@ -1,10 +1,10 @@
 test_that("mockVocabularySet", {
 
   dbName <- "GiBleed"
-  expect_no_error(cdm <- mockVocabularySet(datasetName = dbName))
+  expect_no_error(cdm <- mockVocabularySet(vocabularySet = dbName))
   expect_no_error(omopgenerics::validateCdmArgument(cdm))
 
-  expect_no_error(cdm <- mockVocabularySet(datasetName = dbName))
+  expect_no_error(cdm <- mockVocabularySet(vocabularySet = dbName))
 
   expect_equal(cdm$person |> nrow(),0)
 
