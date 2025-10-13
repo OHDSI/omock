1f7886553b82270cc5f512be2136d697b80d5f10
ilovemane <58779940+ilovemane@users.noreply.github.com>
2025-10-13 09:38:44 +0100

    add reexport

--- R/reexports-omopgenerics.R
+++ R/reexports-omopgenerics.R
@@ -37,3 +37,79 @@ omopgenerics::dropTable
 #' @importFrom omopgenerics insertTable
 #' @export
 omopgenerics::insertTable
+
+#' @importFrom omopgenerics cdmReference
+#' @export
+omopgenerics::cdmReference
+
+#' @importFrom omopgenerics tableName
+#' @export
+omopgenerics::tableName
+
+#' @importFrom omopgenerics tableSource
+#' @export
+omopgenerics::tableSource
+
+#' @importFrom omopgenerics dropSourceTable
+#' @export
+omopgenerics::dropSourceTable
+
+#' @importFrom omopgenerics listSourceTables
+#' @export
+omopgenerics::listSourceTables
+
+#' @importFrom omopgenerics readSourceTable
+#' @export
+omopgenerics::readSourceTable
+
+#' @importFrom omopgenerics omopColumns
+#' @export
+omopgenerics::omopColumns
+
+#' @importFrom omopgenerics omopTables
+#' @export
+omopgenerics::omopTables
+
+#' @importFrom omopgenerics cohortColumns
+#' @export
+omopgenerics::cohortColumns
+
+#' @importFrom omopgenerics cohortTables
+#' @export
+omopgenerics::cohortTables
+
+#' @importFrom omopgenerics achillesColumns
+#' @export
+omopgenerics::achillesColumns
+
+#' @importFrom omopgenerics achillesTables
+#' @export
+omopgenerics::achillesTables
+
+#' @importFrom omopgenerics cohortCodelist
+#' @export
+omopgenerics::cohortCodelist
+
+#' @importFrom omopgenerics suppress
+#' @export
+omopgenerics::suppress
+
+#' @importFrom omopgenerics exportSummarisedResult
+#' @export
+omopgenerics::exportSummarisedResult
+
+#' @importFrom omopgenerics importSummarisedResult
+#' @export
+omopgenerics::importSummarisedResult
+
+#' @importFrom omopgenerics groupColumns
+#' @export
+omopgenerics::groupColumns
+
+#' @importFrom omopgenerics strataColumns
+#' @export
+omopgenerics::strataColumns
+
+#' @importFrom omopgenerics additionalColumns
+#' @export
+omopgenerics::additionalColumns
