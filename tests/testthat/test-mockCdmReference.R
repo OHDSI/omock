82dc391fa9d8a2eb924f60133488115601641739
ilovemane <58779940+ilovemane@users.noreply.github.com>
2025-10-23 16:11:44 +0100

    add test

--- tests/testthat/test-mockCdmReference.R
+++ tests/testthat/test-mockCdmReference.R
@@ -5,3 +5,13 @@ test_that("check it works", {
 
   expect_true(attributes(cdm)$cdm_name == "liverpool")
 })
+
+test_that("check no error with vocab set in mockVocabulary set", {
+
+  expect_no_error(omock::mockCdmReference(cdmName = "liverpool",
+                                 vocabularySet = "GiBleed"))
+
+
+})
+
+
