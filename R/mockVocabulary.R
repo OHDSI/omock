fbdd786cf1ad607bba778550d6260a5ae12bc1a0
ilovemane <58779940+ilovemane@users.noreply.github.com>
2025-10-23 15:49:18 +0100

    fix vocabSet

--- R/mockVocabulary.R
+++ R/mockVocabulary.R
@@ -59,6 +59,12 @@ mockVocabularyTables <- function(cdm = mockCdmReference(),
                                  conceptAncestor = NULL,
                                  drugStrength = NULL) {
 
+  lifecycle::deprecate_warn(
+    "0.6.0",                  # version when deprecated
+    "mockVocabularyTables()",             # deprecated function
+    "mockVocabularySet()"              # replacement
+  )
+
 
 
   #
